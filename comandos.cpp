4986d21a91de03e655c5290833f9000af6e1f5f7
vknaraujo <vitor.aoliveira@sptech.school>
2024-09-09 20:14:02 -0300

    lista de comandos lubuntu (comentado)

---
+++ comandos.cpp
@@ -0,0 +1,41 @@
+    // Criar um usuário.
+    sudo adduser nome_usuario
+
+    // Acessar o usuário criado.
+    su nome_usuario
+
+    // Sair da seção do usuário atual.
+    exit
+
+    // Trocar a senha do usuário.
+    sudo passwd nome_usuario
+
+    // Acessar diretório.
+    cd ~/nome_diretorio
+
+    // Acessar o diretório raiz do usuário.
+    cd ~
+
+    // Listar arquivos no diretório atual.
+    ls
+
+    // Criar diretório.
+    mkdir nome_diretorio
+
+    // Verificar o caminho absoluto que está atualmente.
+    pwd
+
+    // Criar um arquivo.
+    touch nome_arquivo.txt
+
+    // Editar arquivo.
+    nano nome_arquivo.txt
+
+    // Verificar conteúdo do arquivo.
+    cat nome_arquivo.txt
+
+    // Excluir arquivo sem sair do diretório atual.
+    rm nome_diretorio/nome_arquivo.txt
+
+    // Excluir diretório.
+    rmdir nome_diretorio
